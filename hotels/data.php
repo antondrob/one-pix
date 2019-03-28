2aa30f1c262060aba4f7af7139c8eb5bbe8cd628
Dmitry Kragel <dmitry.kragel@gmail.com>
2019-03-29 02:03:52 +0300

    complete JS filter form

---
+++ hotels/data.php
@@ -0,0 +1,15 @@
+<?php
+
+$data = [
+    'aaa' => 'Aaa',
+    'bbb' => 'Bbb',
+    'ccc' => 'Ccc',
+    'ddd' => 'Ddd',
+    'eee' => 'Eee',
+    'fff' => 'Fff',
+    'ggg' => 'Ggg',
+    'hhh' => 'Hhh'
+];
+
+echo json_encode($data);
+
